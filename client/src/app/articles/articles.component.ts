deb4951549fdda9eb7181d467330f2cf33c56b83
Wayne Burris <waterseeker@Hotmail.com>
2020-03-31 11:13:13 -0500

    added ngFor in list of articles, refactored article list into the article component

--- client/src/app/articles/articles.component.ts
+++ client/src/app/articles/articles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ARTICLES } from '../mock-articles';
 
 @Component({
   selector: 'app-articles',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
-
+  articles = ARTICLES;
   constructor() { }
 
+  onSelect(article: Article): void {
+    this.selectedArticle = article;
+  }
   ngOnInit(): void {
   }
 
---
+++ client/src/app/interfaces/article.ts
@@ -0,0 +1,7 @@
+export interface Article {
+    articleId: string;
+    content: string;
+    author: string;
+    visibility: string;
+    title: string;
+  }
