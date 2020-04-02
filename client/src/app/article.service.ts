99dd9a1682b7f59cca2c4a7806936e98f2b0dedb
Wayne Burris <waterseeker@Hotmail.com>
2020-04-02 10:46:05 -0500

    changed linter to run on pull instead of push, working on articles service - currently getting a CORS error

--- client/src/app/article.service.ts
+++ client/src/app/article.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Article } from './interfaces/article';
 import { ARTICLES } from './mock-articles';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
-
-  constructor() { }
+  private articlesUrl = 'http://localhost:3000/api/articles';
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   getArticles(): Observable<Article[]> {
-    return of(ARTICLES);
+    return this.http.get<Article[]>(this.articlesUrl);
   }
 }
