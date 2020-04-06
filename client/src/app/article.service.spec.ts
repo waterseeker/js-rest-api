720cfd5f518b080802c760546e84ea9f8096df8d
Wayne Burris <waterseeker@Hotmail.com>
2020-04-06 10:08:02 -0500

    added httpclientmodule to spec.ts files for article and message services

--- client/src/app/article.service.spec.ts
+++ client/src/app/article.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { ArticleService } from './article.service';
@@ -6,7 +7,9 @@ describe('ArticleService', () => {
   let service: ArticleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(ArticleService);
   });
 
--- client/src/app/message.service.spec.ts
+++ client/src/app/message.service.spec.ts
@@ -1,12 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
   let service: MessageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(MessageService);
   });
 
