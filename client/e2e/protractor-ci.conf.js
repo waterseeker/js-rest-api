1e89dc45ceedd0a5a593ba4d732a1b3ed3b6799c
RJ Beers <r33beers@gmail.com>
2020-04-08 17:32:38 -0400

    Does protractor just suck?

--- client/e2e/protractor-ci.conf.js
+++ client/e2e/protractor-ci.conf.js
@@ -1,11 +1,9 @@
 const config = require('./protractor.conf').config;
 
-config.chromeDriver = process.env.CHROMEWEBDRIVER;
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
     args: ['--headless', '--no-sandbox', '--disable-gpu'],
-    binary: process.env.CHROME_BIN,
   }
 };
 
